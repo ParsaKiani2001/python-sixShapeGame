f87b09ccc4235c2c0aa18c03a7078e2f36daf264
parsa kiani <parsa.kiani.2001@gmail.com>
2024-02-02 09:45:00 +0330

    complete

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ class Board(tk.Tk):
         self.get_button(3,0).config(text = ">",background="green")
         
     def refresh_game(self):
+        change =False
         for button in self._cells:
             if button.cget("text") == "":
                 button.config(state ="disabled",background = "gray")
@@ -47,6 +48,69 @@ class Board(tk.Tk):
         self.get_button(1,4).config(state="disabled")
         self.get_button(2,4).config(state="disabled")
         self.get_button(3,4).config(state="disabled")
+        finishG = 0
+        finishB = 0
+        isLockGreen = True
+        for button in self._cells:
+            if self._cells[button] == (1,4) and button.cget("text") != "":
+                finishG += 1
+            if self._cells[button] == (2,4) and button.cget("text") != "":
+                finishG += 1
+            if self._cells[button] == (3,4) and button.cget("text") != "":
+                finishG += 1
+            if self._cells[button] == (4,1) and button.cget("text") != "":
+                finishB += 1
+            if self._cells[button] == (4,2) and button.cget("text") != "":
+                finishB += 1
+            if self._cells[button] == (4,3) and button.cget("text") != "":
+                finishB += 1
+        if finishB >= 2 or finishG >= 2:
+            for button in self._cells:
+                if self._cells[button] != (1,4) and self._cells[button] != (2,4) and self._cells[button] != (3,4):
+                    if button.cget("text") == ">":
+                        row,col = self._cells[button]
+                        if(self.get_button(row,col+1).cget("text") == "^"):
+                            button2 = self.get_button(row,col+2)
+                            if(button2 is not None):
+                                if(button2.cget("text") == "^"):
+                                  change = True
+                                  isLockGreen = True
+                if self._cells[button] != (4,1) and self._cells[button] != (4,2) and self._cells[button] != (4,3):
+                    if button.cget("text") == "^":
+                        row,col = self._cells[button]
+                        if(self.get_button(row+1,col).cget("text") == ">"):
+                            button2 = self.get_button(row+2,col)
+                            if(button2 is not None):
+                                if(button2.cget("text") == ">"):
+                                  change = True 
+                                  isLockGreen = False
+        if change:
+            if isLockGreen:
+                self.first = True
+            else:
+                self.first = False
+            for button in self._cells:
+                if button.cget("text") == "":
+                    button.config(state ="disabled",background = "gray")
+                else:
+                    button.config(state = "normal")               
+                if self.first:
+                    if button.cget("text") == "^":
+                        button.config(state = "normal")
+                    if button.cget("text") == ">":
+                        button.config(state = "disabled")
+                else:
+                    if button.cget("text") == ">":
+                        button.config(state = "normal")
+                    if button.cget("text") == "^":
+                        button.config(state = "disabled")
+            self.get_button(4,1).config(state="disabled")
+            self.get_button(4,2).config(state="disabled")
+            self.get_button(4,3).config(state="disabled")
+            self.get_button(1,4).config(state="disabled")
+            self.get_button(2,4).config(state="disabled")
+            self.get_button(3,4).config(state="disabled")
+            
         if self.first ==False:
             self.display.config(text="Green",foreground="green")
         else:
@@ -55,10 +119,12 @@ class Board(tk.Tk):
             messagebox.showinfo("end game","blue win")
             self.first = False
             self.start_game()
+            self.refresh_game()
         if self.get_button(1,4).cget("text") == ">" and self.get_button(2,4).cget("text") == ">" and self.get_button(3,4).cget("text") == ">" :
             messagebox.showinfo("end game","green win")
             self.first = False
             self.start_game()
+            self.refresh_game()
         
     
     def jump(self,row,col,row2,col2,key):
@@ -87,18 +153,6 @@ class Board(tk.Tk):
                     if(button3.cget("text")==""):
                         self.jump(row,col,row+2,col,"^")
                     else:
-                        finished = 0
-                        if self.get_button(4,1).cget("text") == "^":
-                            finished =+ 1
-                        if self.get_button(4,2).cget("text") == "^":
-                            finished =+ 1
-                        if self.get_button(4,3).cget("text") == "^":
-                            finished =+ 1
-                        if(finished >= 2):
-                            if self.first:
-                                self.first = False
-                            else:
-                                self.first = True
                         self.refresh_game()
         else:
             button2 = self.get_button(row,col+1)
@@ -110,18 +164,6 @@ class Board(tk.Tk):
                     if(button3.cget("text")==""):
                         self.jump(row,col,row,col+2,">")
                     else:
-                        finished = 0
-                        if self.get_button(1,4).cget("text") == ">":
-                            finished =+ 1
-                        if self.get_button(2,4).cget("text") == ">":
-                            finished =+ 1
-                        if self.get_button(3,4).cget("text") == ">":
-                            finished =+ 1
-                        if(finished >= 2):
-                            if self.first:
-                                self.first = False
-                            else:
-                                self.first = True
                         self.refresh_game()
         
     def _create_board_display(self):
